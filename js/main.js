7fd52718de60c3e8856d7be91f91ec976ac733f6
Valentin Fracassi <ivfracassi@gmail.com>
2023-09-24 23:44:33 -0300

    Version 4.0

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let clientes = []
 let productos = []
-
+let mostrador = document.getElementById("mostrador");
 
 class Producto{
     constructor(nombre, precio, stock){
@@ -19,98 +19,109 @@ class Cliente{
     }
 }
 
+localStorage.clear()
+//Funcion que genera un arreglo de clientes precargados.
+function precargar(clientes){
+    let nombres = ["Carlos", "Raul", "Hector", "Mario", "Ruben", "Carolina", "Mariana", "Ruperta", "Ades", "Zeus", "Poseidon"];
+    let preMail = ["carlitos@gmail.com", "raul@gmail.com", "hectorcito@gmail.com", "marito@gmail.com", "ruby@gmail.com", "caren@gmail.com", "marichu@gmail.com", "rupi@gmail.com", "adesito@gmail.com", "dioszeuselmasgrande@gmail.com", "poseidon@gmail.com"];
+    for (let i = 0; i < nombres.length; i++) {
+        let nom = nombres[i];
+        let num = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000;
+        let indice = Math.floor(Math.random() * preMail.length);
+        let mail = preMail[indice];
+        let nuevo = new Cliente(nom, num, mail);
+        clientes.push(nuevo);
+        let numeroCliente = "Cliente" + i;
+        console.log(numeroCliente)
+        let clienteJSON = JSON.stringify(nuevo);
+        localStorage.setItem(numeroCliente, clienteJSON);
+    }
+    console.log(clientes);
+}
+precargar(clientes)
 
-let nombres = ["Carlos", "Raul", "Hector", "Mario", "Ruben", "Carolina", "Mariana", "Ruperta", "Ades", "Zeus", "Poseidon"];
-let preMail = ["carlitos@gmail.com", "raul@gmail.com", "hectorcito@gmail.com", "marito@gmail.com", "ruby@gmail.com", "caren@gmail.com", "marichu@gmail.com", "rupi@gmail.com", "adesito@gmail.com", "dioszeuselmasgrande@gmail.com", "poseidon@gmail.com"];
 
 
-// Define la clase Cliente aquí si aún no lo has hecho
+//Funcion que esconde el menu de opciones y que permite volver presionando F4.
+let menuVisible = true;
+const menu = document.getElementById('opciones');
+const contenido = document.getElementById('mostrador');
 
-for (let i = 0; i < nombres.length; i++) {
-    let nom = nombres[i];
-    let num = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000;
-    let indice = Math.floor(Math.random() * preMail.length);
-    let mail = preMail[indice];
-    let nuevo = new Cliente(nom, num, mail);
-    clientes.push(nuevo);
-    let numeroCliente = "Cliente" + i;
-    console.log(numeroCliente)
-    let clienteJSON = JSON.stringify(nuevo);
-    localStorage.setItem(numeroCliente, clienteJSON);
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F4' && !menuVisible) {
+        toggleMenu();
+        event.preventDefault();
+    }
+});
+
+function toggleMenu() {
+    menuVisible = !menuVisible;
+    if (menuVisible) {
+        menu.classList.remove('oculto');
+        contenido.classList.add('oculto');
+        contenido.innerHTML = ""
+    } else {
+        menu.classList.add('oculto');
+        contenido.classList.remove('oculto');
+    }
 }
 
-console.log(clientes); // Esto mostrará el arreglo de clientes en la consola
-
+//Opcion 1
 function verClientes() {
-    let mostrador = document.getElementById("mostrador");
-    let clientesRecuperados = [];
-
-    // Iterar a través de las claves del localStorage para recuperar los clientes
-    for (let i = 0; i < localStorage.length; i++) {
-        let clave = localStorage.key(i);
-        let clienteJSON = localStorage.getItem(clave);
-        let cliente = JSON.parse(clienteJSON);
-        clientesRecuperados.push(cliente);
-    }
-
-    if (clientesRecuperados.length <= 0) {
+    if (clientes.length <= 0) {
         mostrador.innerHTML = "No hay ningún cliente cargado. Ingrese a opción 2 (cargar cliente).";
     } else {
         let listaHTML = "<ul>";
-
-        for (let i = 0; i < clientesRecuperados.length; i++) {
-            let cliente = clientesRecuperados[i];
+        for (let i = 0; i < clientes.length; i++) {
+            let cliente = clientes[i];
+            let numeroCliente = "Cliente" + i;
+            let clienteJSON = JSON.stringify(cliente)
+            localStorage.setItem(numeroCliente, clienteJSON)
             listaHTML += "<li>Nombre: " + cliente.nombre + " | Número: " + cliente.numero + " | Email: " + cliente.mail + "</li>";
         }
-
         listaHTML += "</ul>";
         mostrador.innerHTML = listaHTML;
     }
 }
-
 let botonVerCliente = document.getElementById("verClientes");
-let botonLimpiar = document.getElementById("limpiar");
-
 botonVerCliente.addEventListener("click", function () {
     verClientes();
-});
-
-botonLimpiar.addEventListener("click", function () {
-    let mostrador = document.getElementById("mostrador");
-    mostrador.innerHTML = ""; // Borrar el contenido del elemento mostrador
-});
-
-function borrarClientes() {
-    let listaClientes = document.getElementById("listaClientes");
-    listaClientes.innerHTML = ""; // Borrar el contenido del elemento listaClientes
-    clientesMostrados = false; // Establecer la variable global como falsa para indicar que la lista de clientes se ha borrado
-}
-
-let botonVerClientes = document.getElementById("verClientes");
-let botonBorrarClientes = document.getElementById("borrarClientes");
-
-botonVerClientes.addEventListener("click", verClientes);
-botonBorrarClientes.addEventListener("click", borrarClientes);
-
+})
 
 
 //Opcion 2
-function nuevoCliente(clientes) {
-    let nom = prompt("Ingrese nombre del cliente");
-    let num = parseInt(prompt("Ingrese numero de contacto de cliente"));
-    let mail = prompt("Ingrese e-mail del cliente: ");
-    let nuevo = new Cliente(nom, num, mail);
+function agregarFormulario() {
+    const form = document.getElementById("nc")
+    form.classList.remove("invisible")
+    const nombre = document.getElementById('nombre').value;
+    const telefono = document.getElementById('telefono').value;
+    const email = document.getElementById('email').value;
     
-    let mostrador = document.getElementById("mostrador");
-    mostrador.innerHTML += "Datos del cliente<br>-Nombre: " + nom + "<br>-Numero de telefono: " + num + "<br>-Email: " + mail + "<br>";
-    
-    clientes.push(nuevo);
-    
-    mostrador.innerHTML += "¡Cliente agregado con éxito!<br>";
-}
+    if (!nombre || !telefono || !email) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Por favor, complete todos los campos.',
+        });
+        return;
+    }
 
-let registrar = document.getElementById("nuevoCliente")
-registrar.onclick = () => nuevoCliente(clientes)
+    const nuevo = new Cliente(nombre, parseInt(telefono), email);
+    clientes.push(nuevo);
+    Swal.fire({
+        icon: 'success',
+        title: 'Cliente agregado con éxito',
+    });
+    document.getElementById('nombre').value = '';
+    document.getElementById('telefono').value = '';
+    document.getElementById('email').value = '';
+
+let agregarForm = document.getElementById("agregarFormulario")
+agregarForm.addEventListener("click", function(){
+    agregarFormulario()
+})
+
+}
 
 //Opcion 3
 function borrarCliente(clientes){
@@ -132,19 +143,7 @@ function borrarCliente(clientes){
 
 }
 
-//Opcion 4
-function cargarProducto(productos){
-    let art = prompt("Ingrese nombre del articulo: ")
-    let precio = parseFloat(prompt("Ingrese precio del articulo: "))
-    let cant = parseInt(prompt("Ingrese stock del articulo: "))
-    let nuevoProducto = new Producto(art, precio, cant)
-    productos.push(nuevoProducto)
-    alert("Datos del articulo stockeado\n-Articulo: " + art + "\n-Precio: $" + precio + "\n-Existencia: " + cant +"\n¡Producto agregado con exito!")
-}
-let cargarProd = document.getElementById("cargarProducto")
-cargarProd.onclick = () => cargarProducto(productos)
-
-//Opcion 5
-function comprar(clientes, productos) {
-
-}
+let botonBorrarCliente = document.getElementById("borrarCliente");
+botonBorrarCliente.addEventListener("click",function(){
+    borrarCliente(clientes)
+})
